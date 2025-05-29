7256026a0ac3dc766b52051a2c00fce40905ef5c
Namara Allan <archnalan@gmail.com>
2025-05-29 22:39:16 +0300

    creation process started

---
+++ FRELODYUI/FRELODYUI.Web.Client/wwwroot/js/dragdrop.js
@@ -0,0 +1,17 @@
+﻿
+window.initDragAndDrop = function () {
+    // Setup global event listeners for drag and drop
+    console.log("Drag and drop initialized");
+};
+
+window.setDragData = function (key, value) {
+    return function (event) {
+        event.dataTransfer.setData(key, value);
+    };
+};
+
+window.getDragData = function (key) {
+    return function (event) {
+        return event.dataTransfer.getData(key);
+    };
+};
